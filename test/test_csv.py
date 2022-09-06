ad2f9fb0c275d1b88295bdc1b5d5bf9dfc277575
david12white <david12white@gmail.com>
2022-09-06 13:29:18 -0400

    Updated Test Engine
    Updated test engine to correctly exit system and report results of all test cases. Need to work on caching old values and restoring them after the tests next.

--- test/test_csv.py
+++ test/test_csv.py
@@ -1,47 +1,67 @@
 from code.Data import *
 from code.arithmetic import *
-file_name = "data/auto93.csv"
+import sys
+file_name = "../data/auto93.csv"
 
 num_fails = 0
 dump = False
 tests = {}
-eg = "ALL"
+test_cli = "ALL"
 
-def test_engine():
+def test_engine(test):
     # cache/store old status values 
     old_dump = False
 
+    crash = False
+    status = True
     #if the test case exists, execute it
-    if eg in tests:
-        test_passes = tests[eg]()
+    if test in tests:
+        #https://wiki.python.org/moin/HandlingExceptions
+        try:
+            test_passes = tests[test]()
+        except:
+            crash = True
+            test_passes = False
     else:
         print("Invalid test case")
+        return
     #restore old settings
     dump = old_dump
+
+    #print output
     if test_passes:
         msg = "PASS"
+    elif crash:
+        msg = "CRASH"
     else:
         msg = "FAIL"
-    print("!!!!!", msg, eg)
+    print("!!!!!!", msg, test)
+    return test_passes
 
 def test_all():
     global num_fails
-    for _,test in tests.items():
-        if test == test_all:
+    for test_name,test_function in tests.items():
+        if test_function == test_all:
             continue
-        if test() == False:
+        print("\n-----------------------------------")
+        if test_engine(test_name) == False:
             num_fails = num_fails + 1
-    return num_fails == 0
-
+    return True
 tests["ALL"] = test_all
 
-#define and push test functions
-def test_data():
-    d= Data(file_name)
-    print(d)
-    return d != None
+#define and add test functions to dictionary
 
-tests["data"] = test_data
+def test_bad():
+    d=Data()
+    return d != None
+tests["bad"] = test_bad
+
+def test_list():
+    print("\nExamples py -m test_csv -e ...")
+    for name, function in tests.items():
+        print("\t", name)
+    return True
+tests["LIST"] = test_list
 
 def test_arithmetic():
     if(1 == 1):
@@ -49,4 +69,11 @@ def test_arithmetic():
     return False
 tests["arithmetic"] = test_arithmetic
 
-test_engine()
+def test_data():
+    d= Data(file_name)
+    return d != None
+tests["data"] = test_data
+
+test_engine(test_cli)
+#https://docs.python.org/2/library/sys.html
+sys.exit(num_fails)
