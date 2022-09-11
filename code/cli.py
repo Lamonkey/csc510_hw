e708e41a069d80f1b5e50f85daf824339b583343
Lamonkey <88therisingsun@gmail.com>
2022-09-11 18:03:38 -0400

    Update cli.py
    added some comments to help me understand the code

--- code/cli.py
+++ code/cli.py
@@ -19,26 +19,32 @@ class config:
         self.the = {}
         for k, x in re.findall('\n [-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)', help):
             self.the[k] = self.coerce(x)
-
+#use command in help message to set key and value for the variable
     def coerce(self, s):
+  #Processing string into boolean, otherwise just return s1
         def fun(s1):
             if s1.lower()=='true':
                 return True
             if s1.lower()=='false':
                 return False
             return s1
+        #if s is int
         if s.isdigit():
             return int(s)
+        #if s is float
         try:
             float(s)
             return float(s)
+         #if s is string
         except ValueError:
             return fun(re.match("^\s*(.*?)\s*$", s).groups()[0])
 
     def cli(self, args):
         for slot in self.the.keys():
             v = str(self.the[slot])
+        #for loop every args, and override value of the key variable with matched args input
             for x in args:
+          #slot[0] is shortcut for slot, both are acceptable
                 if (x=="-" + slot[0]) or (x=="--" + slot):
                     if v == 'false':
                         v = True
@@ -46,6 +52,7 @@ class config:
                         v = False
                     else:
                         v = args[-1]
+            #override value of the[slot] from args, but doesn't seem can handle multiple args
             self.the[slot] = self.coerce(v)
         if self.the['help']:
             print("\n" + help + "\n")
