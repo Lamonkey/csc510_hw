a115d9b1084aa785b2d9b0b57900c9a1adeea016
Josh <joshuam1008@gmail.com>
2022-09-12 18:29:18 -0400

    updated requirements and added entry skeleton

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 # from https://docs.python.org/3/distutils/setupscript.html
-from distutils.core import setup
-setup(
-    packages=['code']
-    )
+# from distutils.core import setup
+# setup(
+#     packages=['code']
+#     )
 
 # from https://setuptools.pypa.io/en/latest/userguide/entry_point.html
-# from setuptools import setup
+from setuptools import setup
 
-# setup(
-#     # ...,
-#     entry_points={
-#         'console_scripts': [
-#             'cli = code:hello_world',
-#         ]
-#     }
-# )
+setup(
+    name="csv",
+    version="1.0.0",
+    packages=["code", "test"],
+    # entry_points={
+    #     'console_scripts': [
+    #       'code = code.cli:main',  
+    #       'tests = test.text_csv:main'
+    #     ]
+    # }
+)
