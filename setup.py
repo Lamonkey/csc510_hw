9851276a1994c07e5dee19f723cf425fc6cb2b70
Josh <joshuam1008@gmail.com>
2022-09-12 17:55:38 -0400

    updated requirements.txt and gitignore for venv

--- setup.py
+++ setup.py
@@ -3,3 +3,15 @@ from distutils.core import setup
 setup(
     packages=['code']
     )
+
+# from https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+# from setuptools import setup
+
+# setup(
+#     # ...,
+#     entry_points={
+#         'console_scripts': [
+#             'cli = code:hello_world',
+#         ]
+#     }
+# )
