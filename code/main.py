621cb489f37e052790ab9b585a73d03722dfddc7
lamonkey <88therisingsun@gmail.com>
2022-09-12 21:37:50 -0400

    add code and test to sys.path #41

--- code/main.py
+++ code/main.py
@@ -1,6 +1,15 @@
+#add code to search 
 import sys
+from os.path import dirname
+absolute_path = dirname(__file__)
+code_path = "/code"
+test_path = "/test"
+code_full_path = dirname(absolute_path) + code_path
+test_full_path = dirname(absolute_path) + test_path
+sys.path.append(code_full_path)
+sys.path.append(test_full_path)
 from cli import Config
-from test.test_csv import *
+from test_csv import *
 
 # Parse Command Line, Populate "the" Configuration Object
 c = Config()
