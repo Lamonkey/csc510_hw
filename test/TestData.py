ae961d9a9c9d1e6f7364c995fa0ab2a18a0d5780
david12white <david12white@gmail.com>
2022-09-23 16:43:27 -0400

    Added tests for Data, stats

--- test/TestData.py
+++ test/TestData.py
@@ -6,7 +6,7 @@ from Data import *
 from Utility import oo
 
 #load csv file into data
-def test_load_data():
+def test_load_file():
     file_name = "data/auto93.csv"
     d = Data(file_name)
     for n in range(0,10):
@@ -14,4 +14,20 @@ def test_load_data():
         #test a couple values
     return True
 
-tests = [test_load_data]
+#print some stats on columns
+def test_stats():
+    file_name = "data/auto93.csv"
+    #load in data from a file
+    d = Data(file_name)
+
+    #calculate mid for x columns and y columns
+    x_cols = d.cols.x_columns
+    y_cols = d.cols.y_columns
+
+    print("xmid", d.stats(3, x_cols, "mid"))
+    print("xdiv", d.stats(3, x_cols, "div"))
+    print("ymid", d.stats(3, y_cols, "mid"))
+    print("ydiv", d.stats(3, y_cols, "div"))
+
+    return True
+tests = [test_load_file, test_stats]
